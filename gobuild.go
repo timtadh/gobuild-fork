46facd0fb43912252c87bf59931674a0a64c115b
Maurice Gilden <MauriceG@gmx.net>
2009-12-18 14:36:26 +0100

    added license text

--- gobuild.go
+++ gobuild.go
@@ -1,3 +1,7 @@
+// Copyright 2009 by Maurice Gilden. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /*
  gobuild - build tool to automate building go programs
 */
