79f6e5941dcf9f2e5a7cef97abbeff6c16403832
Maurice Gilden <MauriceG@gmx.net>
2010-01-03 11:52:41 +0100

    -t improved (error if no _test.go files and additional console outputs)

--- gobuild_test.go
+++ gobuild_test.go
@@ -1,11 +1,17 @@
 package main_test
 
 import (
-	"fmt";
-	"testing";
+	"fmt"
+	"testing"
 )
 
 func TestGobuild(t *testing.T) {
 	// TODO: do something testy here
-	fmt.Println("Here be some testing someday. Please?");
+	fmt.Println("Here be some testing someday. Please?")
+}
+
+func BenchmarkGobuild(b *testing.B) {
+        b.StopTimer()
+	fmt.Println("Benchmarks would be nice too...")
+        b.StartTimer()
 }
